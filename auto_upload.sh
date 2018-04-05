ef95f839389baa31c86540761bacf569c9336077
zbf <hgkdzbf6@qq.com>
2018-04-06 00:09:01 +0800

    hello world

---
+++ auto_upload.sh
@@ -0,0 +1,24 @@
+cd hector_localization 
+git add .
+git commit -m "$1"
+git push origin +catkin
+cd ../rotors_simulator
+git add .
+git commit -m "$1"
+git push origin +master
+cd ../rpg_svo
+git add .
+git commit -m "$1"
+git push origin +master
+cd ../rotors_utils
+git add .
+git commit -m "$1"
+git push origin +master
+cd ../Sophus
+git add .
+git commit -m "$1"
+git push origin +master
+cd ../sift
+git add .
+git commit -m "$1"
+git push origin +master
